9820b33651f6431eb2f6eeacc17b43ce67a6a65b
jstloyal <jstloyalty@gmail.com>
2020-09-16 16:15:12 +0100

    Add html interface

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
 import './assets/style.css';
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
